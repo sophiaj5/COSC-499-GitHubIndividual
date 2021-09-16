632817f53093dc6577a8d71f5a0b838cbc735a4e
sophiaj5 <59673737+sophiaj5@users.noreply.github.com>
2021-09-15 23:22:59 -0800

    second commit with changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #this is a program to sort numbers in a list
 
-numbers = [7, 13, 2, 6, 68]
-numbers.sort()
-print(numbers)
+numbers = [7, 13, 2, 6, 68] #list of numbers
+numbers.sort() #sorting the list
+print(numbers) #printing the sorted list
